76f314dbc2f7836c257328ffcc572f1aa164b263
NGUYEN TRONG TU <tu.nguyentrong@tpssoft.com>
2025-03-28 15:14:36 +0700

    TextField and Checkbox

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -1,10 +1,49 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController _controller = TextEditingController();
+  bool? isChecked = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: const Text("Profile Page"));
+    return Padding(
+      padding: EdgeInsets.all(20.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: _controller,
+            onEditingComplete: () {
+              setState(() {});
+            },
+          ),
+          Text(_controller.text),
+          Checkbox(
+            value: isChecked,
+            onChanged: (bool? value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+          CheckboxListTile(
+            title: Text('CheckboxListTile'),
+            subtitle: Text('Subtitle'),
+            value: isChecked,
+            onChanged: (value) {
+              setState(() {
+                isChecked = value;
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
