b7f8ffbce279e1fa3c9065dde9b575053f730b52
trongtunguyen08 <trongtunguyen08@icloud.com>
2025-03-30 15:44:41 +0700

    GestureDetector

--- lib/views/pages/profile_page.dart
+++ lib/views/pages/profile_page.dart
@@ -17,54 +17,62 @@ class _ProfilePageState extends State<ProfilePage> {
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(20.0),
-      child: Column(
-        children: [
-          TextField(
-            controller: _controller,
-            onEditingComplete: () {
-              setState(() {});
-            },
-          ),
-          Text(_controller.text),
-          Checkbox(
-            value: isChecked,
-            onChanged: (bool? value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          CheckboxListTile(
-            title: Text('CheckboxListTile'),
-            subtitle: Text('Subtitle'),
-            value: isChecked,
-            onChanged: (value) {
-              setState(() {
-                isChecked = value;
-              });
-            },
-          ),
-          SwitchListTile(
-            title: Text('SwitchListTile'),
-            subtitle: Text('Subtitle'),
-            controlAffinity: ListTileControlAffinity.leading,
-            value: isSwitched,
-            onChanged: (value) {
-              setState(() {
-                isSwitched = value;
-              });
-            },
-          ),
-          Slider(
-            value: sliderValue,
-            onChanged: (value) {
-              setState(() {
-                sliderValue = value;
-              });
-            },
-            divisions: 10,
-          ),
-        ],
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            TextField(
+              controller: _controller,
+              onEditingComplete: () {
+                setState(() {});
+              },
+            ),
+            Text(_controller.text),
+            Checkbox(
+              value: isChecked,
+              onChanged: (bool? value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            CheckboxListTile(
+              title: Text('CheckboxListTile'),
+              subtitle: Text('Subtitle'),
+              value: isChecked,
+              onChanged: (value) {
+                setState(() {
+                  isChecked = value;
+                });
+              },
+            ),
+            SwitchListTile(
+              title: Text('SwitchListTile'),
+              subtitle: Text('Subtitle'),
+              controlAffinity: ListTileControlAffinity.leading,
+              value: isSwitched,
+              onChanged: (value) {
+                setState(() {
+                  isSwitched = value;
+                });
+              },
+            ),
+            Slider(
+              value: sliderValue,
+              onChanged: (value) {
+                setState(() {
+                  sliderValue = value;
+                });
+              },
+              divisions: 10,
+            ),
+            GestureDetector(
+              child: Image.asset("assets/images/bg.jpg"),
+              onTap: () {
+                print("Image selected");
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
