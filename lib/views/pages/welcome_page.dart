dd8a7c1c365c75b4ac7ea13a75a72c723231f0b1
trongtunguyen08 <trongtunguyen08@icloud.com>
2025-04-06 13:40:58 +0700

    Ui Modification

--- lib/views/pages/welcome_page.dart
+++ lib/views/pages/welcome_page.dart
@@ -1,5 +1,4 @@
-import 'package:demo_flutter/views/widget_tree.dart';
-import 'package:demo_flutter/views/widgets/hero_widget.dart';
+import 'package:demo_flutter/views/pages/login_page.dart';
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
@@ -9,37 +8,54 @@ class WelcomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Lottie.asset("assets/lotties/welcome.json"),
-            FittedBox(
-              child: Text(
-                "Flutter Mapp",
-                style: TextStyle(fontSize: 50.0, letterSpacing: 50.0),
-              ),
-            ),
-            SizedBox(height: 20.0),
-            FilledButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                  context,
-                  MaterialPageRoute(
-                    builder: (context) {
-                      return WidgetTree();
-                    },
+      body: Center(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Lottie.asset("assets/lotties/welcome.json", height: 400.0),
+                FittedBox(
+                  child: Text(
+                    "Flutter Mapp",
+                    style: TextStyle(fontSize: 50.0, letterSpacing: 50.0),
                   ),
-                );
-              },
-              style: FilledButton.styleFrom(
-                minimumSize: Size(double.infinity, 40.0),
-              ),
-              child: Text("Get Started"),
+                ),
+                SizedBox(height: 20.0),
+                FilledButton(
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                        builder: (context) {
+                          return LoginPage(title: "Register");
+                        },
+                      ),
+                    );
+                  },
+                  style: FilledButton.styleFrom(
+                    minimumSize: Size(double.infinity, 40.0),
+                  ),
+                  child: Text("Get Started"),
+                ),
+                TextButton(
+                  onPressed:
+                      () => {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) {
+                              return LoginPage(title: "Login");
+                            },
+                          ),
+                        ),
+                      },
+                  child: const Text("Login"),
+                ),
+              ],
             ),
-            TextButton(onPressed: () => {}, child: const Text("Login")),
-          ],
+          ),
         ),
       ),
     );
--- lib/views/widgets/hero_widget.dart
+++ lib/views/widgets/hero_widget.dart
@@ -1,16 +1,38 @@
 import 'package:flutter/material.dart';
 
 class HeroWidget extends StatelessWidget {
-  const HeroWidget({super.key});
+  const HeroWidget({super.key, required this.title});
+
+  final String title;
 
   @override
   Widget build(BuildContext context) {
-    return Hero(
-      tag: "bg",
-      child: ClipRRect(
-        borderRadius: BorderRadius.circular(20.0),
-        child: Image.asset("assets/images/bg.jpg"),
-      ),
+    return Stack(
+      alignment: Alignment.center,
+      children: [
+        Hero(
+          tag: "bg",
+          child: ClipRRect(
+            borderRadius: BorderRadius.circular(20.0),
+            child: Image.asset(
+              "assets/images/bg.jpg",
+              colorBlendMode: BlendMode.darken,
+              color: Colors.teal,
+            ),
+          ),
+        ),
+        FittedBox(
+          child: Text(
+            title,
+            style: TextStyle(
+              color: Colors.white30,
+              fontWeight: FontWeight.bold,
+              fontSize: 50.0,
+              letterSpacing: 50.0,
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
